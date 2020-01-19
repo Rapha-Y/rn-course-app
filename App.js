af4e2ed697a38c2a3251fb87e61da005c388f445
raphyuki <raph.yuki@gmail.com>
2020-01-19 21:38:21 -0200

    added text input

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import React, { useState } from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
-  const [outputText, setOutputText] = useState('Open up App.js to start working on your app!');
   return (
-    <View style={styles.container}>
-      <Text>{outputText}</Text>
-      <Button onPress={() => setOutputText('The text changed!')} title="Change Text" />
+    <View style={{paddingHorizontal: 20 , paddingVertical: 40}}>
+      <View style={{alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between'}}>
+        <TextInput placeholder="New to-do item" style={{borderBottomColor: 'black', borderWidth: 1, flex: 1, paddingHorizontal: 10, paddingVertical: 5}} />
+        <Button style={{flex: 1}} title="add" />
+      </View>
+      <View>
+
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  
 });
