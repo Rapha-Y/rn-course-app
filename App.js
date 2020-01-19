a9c56f0eb95cdaada9aac621f18ae0e5beeb9592
raphyuki <raph.yuki@gmail.com>
2020-01-19 21:44:28 -0200

    separated components from styling

--- App.js
+++ App.js
@@ -3,10 +3,10 @@ import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{paddingHorizontal: 20 , paddingVertical: 40}}>
-      <View style={{alignItems: 'center', flexDirection: 'row', justifyContent: 'space-between'}}>
-        <TextInput placeholder="New to-do item" style={{borderBottomColor: 'black', borderWidth: 1, flex: 1, paddingHorizontal: 10, paddingVertical: 5}} />
-        <Button style={{flex: 1}} title="add" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="New to-do item" style={styles.textInput} />
+        <Button style={styles.inputButton} title="add" />
       </View>
       <View>
 
@@ -16,5 +16,23 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  
+  inputButton: {
+    flex: 1,
+  },
+  inputContainer: {
+    alignItems: 'center', 
+    flexDirection: 'row', 
+    justifyContent: 'space-between',
+  },
+  screen: {
+    paddingHorizontal: 20, 
+    paddingVertical: 40,
+  },
+  textInput: {
+    borderBottomColor: 'black', 
+    borderWidth: 1, 
+    flex: 1, 
+    paddingHorizontal: 10, 
+    paddingVertical: 5,
+  },
 });
