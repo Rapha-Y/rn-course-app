930dab4e59040f118682dffd08db9b8e7d3ad8e5
raphyuki <raph.yuki@gmail.com>
2020-01-21 19:53:01 -0200

    added modal for input components

--- components/TodoInput.js
+++ components/TodoInput.js
@@ -1,38 +1,54 @@
 import React, { useState } from 'react';
-import { Button, StyleSheet, TextInput, View, } from 'react-native';
+import { Button, Modal, StyleSheet, TextInput, View, } from 'react-native';
 
 const TodoInput = props => {
-    const [enteredTodo, setNewTodoItem] = useState('');
+    const [enteredTodo, setEnteredTodo] = useState('');
 
     const todoInputHandler = (enteredText) => {
-        setNewTodoItem(enteredText);
+        setEnteredTodo(enteredText);
+    };
+
+    const addTodoHandler = () => {
+        props.onAddTodo(enteredTodo);
+        setEnteredTodo('');
     };
 
     return (
-        <View style={styles.inputContainer}>
-            <TextInput 
-                onChangeText={todoInputHandler} 
-                placeholder="New to-do item" 
-                style={styles.textInput} 
-                value={enteredTodo}
-            />
-            <Button 
-                onPress={props.onAddTodo.bind(this, enteredTodo)}
-                title="add" 
-            />
-        </View>
+        <Modal animationType="slide" visible={props.visible}>
+            <View style={styles.inputContainer}>
+                <TextInput 
+                    onChangeText={todoInputHandler} 
+                    placeholder="New to-do item" 
+                    style={styles.textInput} 
+                    value={enteredTodo}
+                />
+                <View style={styles.buttonContainer}>
+                    <Button color="red" onPress={props.onCancel} title="cancel" />
+                    <Button 
+                        onPress={addTodoHandler}
+                        title="add" 
+                    />
+                </View>
+            </View>
+        </Modal>
     );
 };
 
 const styles = StyleSheet.create({
+    buttonContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        width: '82%',
+    },
     inputContainer: {
         alignItems: 'center', 
-        flexDirection: 'row', 
-        justifyContent: 'space-between',
+        flex: 1,
+        justifyContent: 'center',
     },
     textInput: {
         borderBottomColor: 'black', 
         borderWidth: 1,
+        marginBottom: 10,
         paddingHorizontal: 10, 
         paddingVertical: 5,
         width: '82%',
